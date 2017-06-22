d744b29a079b931e2984cb0e70e6124ebeb53ac4
jinfeng.niu <jinfeng.niu@phicomm.com>
2017-06-22 20:14:38 +0800

    初始化提交

---
+++ 斗鱼直播/斗鱼直播/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  斗鱼直播
+//
+//  Created by niujinfeng on 2017/6/22.
+//  Copyright © 2017年 niujinfeng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
